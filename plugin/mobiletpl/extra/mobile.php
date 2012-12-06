f8299742651919972c1dea9120a737c4f3e80354
Mzhelskiy Maxim <rus.engine@gmail.com>
2012-12-06 15:20:37 +0400

    update css and js

--- plugin/mobiletpl/extra/mobile.php
+++ plugin/mobiletpl/extra/mobile.php
@@ -1,3 +1,14 @@
 <?php
 
-require_once(Config::get('path.root.server').'/plugins/mobiletpl/extra/init.php');
+/**
+ * Этот файл необходимо скопировать в каталог /include/ вашего сайта
+ * Суть его в том, чтобы подменять список активированных плагинов, это позволяет избежать потенциальных ошибок в мобильном шаблоне
+ */
+
+/**
+ * Подключаем файл инициализации мобильного шаблона
+ */
+$sFileInit=Config::get('path.root.server').'/plugins/mobiletpl/extra/init.php';
+if (file_exists($sFileInit)) {
+	require_once($sFileInit);
+}
