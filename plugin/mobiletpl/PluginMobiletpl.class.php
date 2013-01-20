bd738bae51756267b9bf951379f9744a289232e5
Mzhelskiy Maxim <rus.engine@gmail.com>
2013-01-20 13:09:35 +0400

    Избавляемся от переопределения списка плагинов. Теперь в мобильной версии всегда активный все плагины основной версии.

--- plugin/mobiletpl/PluginMobiletpl.class.php
+++ plugin/mobiletpl/PluginMobiletpl.class.php
@@ -23,70 +23,12 @@ if (!class_exists('Plugin')) {
 }
 
 class PluginMobiletpl extends Plugin {
-
-	protected $aInherits=array(
-
-	);
-
-	public function __construct() {
-		/**
-		 * Если сейчас активна мобильная версия, то отключаем доступ к админке
-		 */
-		if (Config::Get('sys.plugins.activation_file')=='plugins-mobile.dat') {
-			$this->aInherits=array(
-				'action' =>array(
-					'ActionAdmin'
-				),
-			);
-		}
-	}
 	
 	/**
 	 * Активация плагина	 
 	 */
 	public function Activate() {
-		$bError=true;
-		/**
-		 * Проверяем наличие файла /plugins/plugins-mobile.dat
-		 */
-		$sPluginFile=Config::get('path.root.server').'/plugins/plugins-mobile.dat';
-		if (!file_exists($sPluginFile)) {
-			/**
-			 * Если файла нет, то пытаемся его создать
-			 */
-			$aPluginsActive=array('mobiletpl');
-			/**
-			 * Записываем данные в файл PLUGINS.DAT
-			 */
-			if (@file_put_contents($sPluginFile, implode(PHP_EOL,$aPluginsActive))!==false) {
-				$bError=false;
-			} else {
-				$this->Message_AddError('Ошибка активации плагина Mobile Template. Необходимо дать права на запись в каталог /plugins/',$this->Lang_Get('error'),true);
-				return false;
-			}
-		} else {
-			$bError=false;
-		}
-		if (!$bError) {
-			/**
-			 * Проверяем наличие файла /include/mobile.php
-			 */
-			$sFile=Config::get('path.root.server').'/include/mobile.php';
-			if (!file_exists($sFile)) {
-				/**
-				 * Пытаемся скопировать необходимый файл
-				 */
-				if (@copy(dirname(__FILE__).'/extra/mobile.php',$sFile)) {
-					return true;
-				} else {
-					$this->Message_AddError('Ошибка активации плагина Mobile Template. Необходимо в каталог /include/ скопировать файл /plugins/mobiletpl/extra/mobile.php',$this->Lang_Get('error'),true);
-					return false;
-				}
-			} else {
-				return true;
-			}
-		}
-		return false;
+		return true;
 	}
 	
 	/**
--- plugin/mobiletpl/classes/modules/main/Main.class.php
+++ plugin/mobiletpl/classes/modules/main/Main.class.php
@@ -18,64 +18,9 @@
 class PluginMobiletpl_ModuleMain extends Module {
 
 	protected $oMapper;
-	protected $sApiUrl='http://phd.yandex.net/detect';
-	protected $aHeaderForDetect=array(
-		'user-agent','profile','wap-profile','x-wap-profile','x-operamini-phone-ua'
-	);
 
 	public function Init() {
 		$this->oMapper=Engine::GetMapper(__CLASS__);
-	}
-	/**
-	 * Выполняет запрос к API сервиса
-	 *
-	 * @param array $aRequest	Список параметров
-	 *
-	 * @return bool|array
-	 */
-	public function RequestApi($aRequest) {
-		$s='';
-		if (is_string($aRequest) or count($aRequest)){
-			$s='?'.(is_array($aRequest) ? http_build_query($aRequest,'','&') : $aRequest);
-		}
-		if ($sReturn=@file_get_contents($this->sApiUrl.'/'.$s)) {
-			return $sReturn;
-		}
-		return false;
-	}
-
-	public function DetectMobileDevice() {
-		$aRequest=array();
-		if (isset($_SERVER)) {
-			$aServer=$_SERVER;
-		} else {
-			$aServer=array();
-		}
-
-		foreach ($aServer as $sKey => $sValue) {
-			if (strpos($sKey,'HTTP_')===0) {
-				$sKey=strtolower(strtr(substr($sKey, 5), '_', '-'));
-				if (in_array($sKey,$this->aHeaderForDetect)) {
-					$aRequest[$sKey]=$sValue;
-				}
-			}
-		}
-		$aRequest['user-agent']='Alcatel-CTH3/1.0 UP.Browser/6.2.ALCATEL MMP/1.0';
-		$sResult=$this->RequestApi($aRequest);
-		$oXml=@simplexml_load_string($sResult);
-		if ((is_object($oXml) == false)) {
-			return false;
-		}
-		if ($oXml->name and (int)$oXml->screenx<1024) {
-			return true;
-		}
-		return false;
-	}
-
-	public function IsNeedShowMobile() {
-		$bDetect=$this->DetectMobileDevice();
-
-		return $bDetect;
 	}
 
 	public function GetTopicLastbyUserId($iUserId) {
